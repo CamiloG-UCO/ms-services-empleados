dead3a810abc97cd53fa68d2cafa0ca2a4acb056
Camilo Garcia <juan092005ca@gmail.com>
2025-10-16 20:40:19 -0500

    First commit

---
+++ src/main/java/co/edu/hotel/empleadoservice/MsServicesEmpleadoApplication.java
@@ -0,0 +1,13 @@
+package co.edu.hotel.empleadoservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsServicesEmpleadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsServicesEmpleadoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/edu/hotel/empleadoservice/MsServicesEmpleadoApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.hotel.empleadoservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsServicesEmpleadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
