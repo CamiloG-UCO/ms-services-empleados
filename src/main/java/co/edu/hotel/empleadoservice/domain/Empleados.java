05adbea11714a488f565362c4f371d6d6d682b5b
luis-ospina-garcia <luisospi1541@gmail.com>
2025-10-23 16:40:52 -0500

    creacion del domain

---
+++ src/main/java/co/edu/hotel/empleadoservice/domain/Empleados.java
@@ -0,0 +1,59 @@
+package co.edu.hotel.empleadoservice.domain;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Data
+@Entity
+@Table(name = "empleados")
+public class Empleados {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(unique = true, nullable = false)
+    private String code;
+
+    @Column(nullable = false)
+    private int identificationNumber;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "tipo_identificacion_id", nullable = false)
+    private TipoIdentificacion identificationType;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "role_id", nullable = false)
+    private Roles role;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private int contactNumber;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private BigDecimal salary;
+
+    @Column(nullable = false)
+    private String hotel;
+
+    @CreationTimestamp
+    @Column(name = "registrar_date", nullable = false)
+    private LocalDate registrationDate;
+
+    @Column(name = "registrar_by", nullable = false)
+    private String registeredBy;
+}
+
---
+++ src/main/java/co/edu/hotel/empleadoservice/domain/TipoIdentificacion.java
@@ -0,0 +1,18 @@
+package co.edu.hotel.empleadoservice.domain;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import java.util.UUID;
+
+@Data
+@Entity
+@Table(name = "identification_types")
+public class TipoIdentificacion {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(unique = true, nullable = false)
+    private String name;
+}
