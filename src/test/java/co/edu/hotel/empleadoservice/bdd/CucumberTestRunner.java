842092ea76a48871ec5ab43be71c22bf4cee37d8
Juan José <juanjq.2244@gmail.com>
2025-10-30 15:04:24 -0500

    adición del Cucumber Test Runner

---
+++ src/test/java/co/edu/hotel/empleadoservice/bdd/CucumberTestRunner.java
@@ -0,0 +1,36 @@
+package co.edu.hotel.empleadoservice.bdd;
+
+import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.Suite;
+
+import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
+import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
+import static io.cucumber.junit.platform.engine.Constants.FEATURES_PROPERTY_NAME;
+
+/**
+ * Runner principal para ejecutar todas las pruebas BDD con Cucumber.
+ *
+ * Ejecutar con:
+ * - Maven: mvn test
+ * - IDE: Run as JUnit Test
+ */
+@Suite
+@IncludeEngines("cucumber")
+@SelectClasspathResource("features")
+@ConfigurationParameter(
+        key = FEATURES_PROPERTY_NAME,
+        value = "classpath:features"
+)
+@ConfigurationParameter(
+        key = GLUE_PROPERTY_NAME,
+        value = "co.edu.hotel.empleadoservice.bdd.steps"
+)
+@ConfigurationParameter(
+        key = PLUGIN_PROPERTY_NAME,
+        value = "pretty, html:target/cucumber-reports/cucumber.html, json:target/cucumber-reports/cucumber.json"
+)
+public class CucumberTestRunner {
+    // Esta clase no necesita código, solo las anotaciones
+}
